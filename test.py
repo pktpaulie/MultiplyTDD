f8a32971cf6dacd27450973d7607c045e84d83a5
Pauline Korukundo <pktpaulie@gmail.com>
2023-06-25 01:46:26 +0300

    TEST FIRST TDD CYCLE 5 Run test 5 and Pass

--- test.py
+++ test.py
@@ -13,4 +13,9 @@ class MultiplyTest(unittest.TestCase):
         """calling the assertEqual function"""
         
 
-        self.assertEqual(multiply(4, 4), 16)
+        self.assertEqual(multiply(23, 45), 23*45)
+
+
+if __name__ == '__main__':
+    
+    unittest.main() #To run this file directly
