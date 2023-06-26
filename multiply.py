8feb901d73a93c70f501658f11ff142b9efc92ef
Pauline Korukundo <pktpaulie@gmail.com>
2023-06-26 19:27:31 +0300

    ADD MULTIPLY FUNCTION USING FOR LOOP

--- multiply.py
+++ multiply.py
@@ -5,3 +5,29 @@ def multiply(var_a, var_b):
     """Function to multiply"""
 
     return var_a * var_b
+
+
+def multiply_new(var_a, var_b):
+    """
+    Function to multiply two numbers using a for loop
+    2 * 3 = 2 + 2 + 2
+    """
+
+    result = 0
+
+
+    for x in range(var_b):
+        if var_b < 0:
+            result = result - var_a
+        else:
+            result = result + var_a
+
+    return result
+
+
+"""
+    x = var_b - 1
+    while x != 0:
+        result = result + var_a
+        x = x - 1
+"""
--- test_multiply.py
+++ test_multiply.py
@@ -1,10 +1,23 @@
 """Using the pytest method for unit testing"""
 
 
-from multiply import multiply
+from multiply import multiply, multiply_new
+
+def test_multiply_1():
+    """calling the assert function"""
+
+    assert multiply(1, 1) == 1
+    assert multiply(2, 2) == 4
+    assert multiply(3, 3) == 9
+    assert multiply(4, 4) == 16
+    assert multiply(23, 45) == 1035
 
 
-def test_multiply():
-    """calling the assertEqual function"""
 
-    assert multiply(23, 45) == 23 * 45
+def test_multiply_new():
+    """
+    calling the assert function to test the 
+    second multiply function
+    """
+
+    assert multiply_new(-3, 3) == -9
